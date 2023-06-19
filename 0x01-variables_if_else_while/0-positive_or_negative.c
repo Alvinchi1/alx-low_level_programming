e600b1b838d20a43477e899fb765f21fe9fa82f5
Alvinchi1 <Alvinchibundo007@gmail.com>
2023-06-19 14:57:05 -0700

    PnN

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,9 +20,12 @@ int main (void)
 		() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%i is positive\n", n);
-	else if (n < 0)
-		printf("%i is negative\n", n);
+        else if (n == 0)
+		prinf("%i is zero\n", n);
 	else
-		printf("%i is zero\n", n);
+		printf("%i is negative\n", n);
+
 	return (0);
+
+
 }
