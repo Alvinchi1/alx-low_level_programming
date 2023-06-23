33051ff8403309cc403e5c8d8ef9ab6abcd529a1
Alvinchi1 <Alvinchibundo007@gmail.com>
2023-06-23 10:07:42 -0700

    debug

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -17,9 +17,9 @@ int main(void)
 
 	largest = largets_number(a, b, c);
 
-	printf(%d is the largest number\n", largest);
+	printf("%d is the largest number\n", largest);
 
-	return 0;
+	return (0);
 
 }
 
